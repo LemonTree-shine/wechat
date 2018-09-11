7e7a0e378037aad64c6aa13e60947387e5a950a5
chenze <chenze@wdai.com>
2018-09-11 14:09:04 +0800

    asd

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ server.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin,Content-Type, Content-Length');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', true);
+
+    //获取token值
+    request(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`, function (error, response, body) {
+        global.wechat_access_token = JSON.parse(body).access_token;
+        // 获取jsapi_ticket
+        var ticketUrl = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + body.access_token + '&type=jsapi';
+        request(ticket, function (err, response, body) {
+            var data = JSON.parse(body);
+            console.log(data);
+            if (data.errcode == 0) {
+                // 这里我缓存到了global
+                global.jsapi_ticket = data.ticket;
+                console.log(global.wechat_access_token,global.jsapi_ticket);
+                // 计算signature
+                // ...
+            }
+        })
+    })
     next();
 });
 
@@ -45,110 +63,106 @@ server.get("/", function (req, res) {
 server.post("/", function (req, res) {
     var appid = "wx6e3bf6cb641b5d35";
     var secret = "b9dff0e88a68b4a818d065d4ea8d5c35";
-    request(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`, function (error, response, body) {
-        global.wechat_access_token = JSON.parse(body).access_token;
-        //res.send("获取成功");
-        try {
-            var buffer = [];
-            //监听 data 事件 用于接收数据
-            req.on('data', function (data) {
-                buffer.push(data);
-            });
-            //监听 end 事件 用于处理接收完成的数据
-            req.on('end', function () {
-                //输出接收完成的数据
-                parseString(Buffer.concat(buffer).toString('utf-8'), { explicitArray: false }, function (err, result) {
-                    if (err) {
-                        //打印错误信息
-                        console.log(err);
-                    } else {
-                        //打印解析结果
-                        console.log(result);
-                        result = result.xml;
-                        var toUser = result.ToUserName; //接收方微信
-                        var fromUser = result.FromUserName;//发送仿微信
-                        //判断是否是事件类型
-                        if (result.Event) {
-                            if (result.Event === 'subscribe') {
-                                //回复消息
-                                var xml = returntext(fromUser, toUser, '欢迎关注公众号,hahhh');
-                                res.send(xml);
-                            }
-                        } else {
-                            /**
-                             * text:消息回复
-                             * image:图片回复
-                            */
-                            switch (result.MsgType) {
-                                case "text":
-                                    if (message[result.Content]) {
-                                        var xml = returntext(fromUser, toUser, message[result.Content]);
-                                    } else {
-                                        var xml = returntext(fromUser, toUser, '后期会增加更多功能');
-                                        //var xml = returnlink(fromUser,toUser,"测试aaa","描述AAA",result.Content,"293845736")
-                                    }
-                                    res.send(xml);
-                                    break;
-                                case "image":
-                                    
-                                    new Promise(function (resolve, reject) {
-                                        var formData = {
-                                            media: fs.createReadStream(__dirname+'/1.png'),
-                                        };
-                                        request.post(`https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${global.wechat_access_token}&type=image`, {
-                                            formData: formData,
-                                        }, (err, httpResponse, body) => {
-                                            console.log(body)
-                                            resolve(body);
-                                        });
-                                    }).then(function (data) {
-                                        // request(`https://api.weixin.qq.com/cgi-bin/media/get?access_token=${global.wechat_access_token}&media_id=${JSON.parse(data).media_id}`, function (error, response, body) {
-                                        //     console.log(11111111111111);
-                                        //     console.log(JSON.parse(data).media_id);
-                                        // })
-                                        var xml = returnimage(fromUser, toUser, JSON.parse(data).media_id);
-                                        console.log(11111111111111);
-                                        console.log(xml)
-                                        res.send(xml);
-                                        
-                                    });
-                                    //returnimage()
-                                    break;
-                                case "voice":
-                                    var xml = returntext(fromUser, toUser, '测试是语音类型类型');
-                                    res.send(xml);
-                                    break;
-                                case "video":
-                                    var xml = returntext(fromUser, toUser, '测试是视频类型类型');
-                                    res.send(xml);
-                                    break;
-                                case "shortvideo":
-                                    var xml = returntext(fromUser, toUser, '测试是小视频类型类型');
-                                    res.send(xml);
-                                    break;
-                                case "location":
-                                    var xml = returntext(fromUser, toUser, '测试是地理位置类型类型');
-                                    res.send(xml);
-                                    break;
-                                case "link":
-                                    var xml = returntext(fromUser, toUser, '测试是链接类型类型');
-                                    res.send(xml);
-                                    break;
-
-                                default:
-                                    var xml = returntext(fromUser, toUser, '回复的格式暂时不支持！');
-                                    res.send(xml);
-                            }
+    try {
+        var buffer = [];
+        //监听 data 事件 用于接收数据
+        req.on('data', function (data) {
+            buffer.push(data);
+        });
+        //监听 end 事件 用于处理接收完成的数据
+        req.on('end', function () {
+            //输出接收完成的数据
+            parseString(Buffer.concat(buffer).toString('utf-8'), { explicitArray: false }, function (err, result) {
+                if (err) {
+                    //打印错误信息
+                    console.log(err);
+                } else {
+                    //打印解析结果
+                    console.log(result);
+                    result = result.xml;
+                    var toUser = result.ToUserName; //接收方微信
+                    var fromUser = result.FromUserName;//发送仿微信
+                    //判断是否是事件类型
+                    if (result.Event) {
+                        if (result.Event === 'subscribe') {
+                            //回复消息
+                            var xml = returntext(fromUser, toUser, '欢迎关注公众号,hahhh');
+                            res.send(xml);
                         }
+                    } else {
+                        /**
+                         * text:消息回复
+                         * image:图片回复
+                        */
+                        switch (result.MsgType) {
+                            case "text":
+                                if (message[result.Content]) {
+                                    var xml = returntext(fromUser, toUser, message[result.Content]);
+                                } else {
+                                    var xml = returntext(fromUser, toUser, '后期会增加更多功能');
+                                    //var xml = returnlink(fromUser,toUser,"测试aaa","描述AAA",result.Content,"293845736")
+                                }
+                                res.send(xml);
+                                break;
+                            case "image":
+                                
+                                new Promise(function (resolve, reject) {
+                                    var formData = {
+                                        media: fs.createReadStream(__dirname+'/1.png'),
+                                    };
+                                    request.post(`https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${global.wechat_access_token}&type=image`, {
+                                        formData: formData,
+                                    }, (err, httpResponse, body) => {
+                                        console.log(body)
+                                        resolve(body);
+                                    });
+                                }).then(function (data) {
+                                    // request(`https://api.weixin.qq.com/cgi-bin/media/get?access_token=${global.wechat_access_token}&media_id=${JSON.parse(data).media_id}`, function (error, response, body) {
+                                    //     console.log(11111111111111);
+                                    //     console.log(JSON.parse(data).media_id);
+                                    // })
+                                    var xml = returnimage(fromUser, toUser, JSON.parse(data).media_id);
+                                    console.log(11111111111111);
+                                    console.log(xml)
+                                    res.send(xml);
+                                    
+                                });
+                                //returnimage()
+                                break;
+                            case "voice":
+                                var xml = returntext(fromUser, toUser, '测试是语音类型类型');
+                                res.send(xml);
+                                break;
+                            case "video":
+                                var xml = returntext(fromUser, toUser, '测试是视频类型类型');
+                                res.send(xml);
+                                break;
+                            case "shortvideo":
+                                var xml = returntext(fromUser, toUser, '测试是小视频类型类型');
+                                res.send(xml);
+                                break;
+                            case "location":
+                                var xml = returntext(fromUser, toUser, '测试是地理位置类型类型');
+                                res.send(xml);
+                                break;
+                            case "link":
+                                var xml = returntext(fromUser, toUser, '测试是链接类型类型');
+                                res.send(xml);
+                                break;
 
-                        console.log(xml);
+                            default:
+                                var xml = returntext(fromUser, toUser, '回复的格式暂时不支持！');
+                                res.send(xml);
+                        }
                     }
-                })
-            });
-        } catch (err) {
-            console.log(error);
-        }
-    })
+
+                    console.log(xml);
+                }
+            })
+        });
+    } catch (err) {
+        console.log(error);
+    }
 })
 
 server.use(express.static(__dirname, ''));
