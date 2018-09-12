83719945b072137ec3367ac17fceac560f32dbe6
chenze <chenze@wdai.com>
2018-09-12 19:34:04 +0800

    a

--- server.js
+++ server.js
@@ -80,33 +80,13 @@ server.get("/", function (req, res) {
 });
 
 server.post("/", function (req, res) {
+    console.log(global.wechat_access_token);
     request.post(` https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${global.wechat_access_token}`, {
         "button": [
             {
                 "type": "click",
                 "name": "今日歌曲",
                 "key": "V1001_TODAY_MUSIC"
-            },
-            {
-                "name": "菜单",
-                "sub_button": [
-                    {
-                        "type": "view",
-                        "name": "搜索",
-                        "url": "http://www.soso.com/"
-                    },
-                    {
-                        "type": "miniprogram",
-                        "name": "wxa",
-                        "url": "http://mp.weixin.qq.com",
-                        "appid": "wx286b93c14bbf93aa",
-                        "pagepath": "pages/lunar/index"
-                    },
-                    {
-                        "type": "click",
-                        "name": "赞一下我们",
-                        "key": "V1001_GOOD"
-                    }]
             }]
     })
     try {
